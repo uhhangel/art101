e96554bd1d9b237ed217042045da3c79dccc0ea7
uhhangel <angelleeanna.b25@gmail.com>
2024-10-17 13:02:34 -0700

    lab 4 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock Paper Scissors
+// An implementation of the game
+// Author: Angelleeanna Bingham
+// Date: 10/16/2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Get 2 willing players.
+// Each player forms a fist with their hand underneath.
+// Each player hits their fist against their palm three times.
+// Mentally, each player silently decides whether to do a rock, paper, or scissors motion with their hand.
+// Players then count to three and display their motion.
+  // Hand beats rock
+  // Scissors beats paper
+  // Rock beats scissors
+  // Paper beats rock
+  // Scissors beats hand
+// Players do a best of three, whoever wins two rounds or three in a row is the winner.
