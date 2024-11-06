a336ad81d51902d8b09781dd0d8bc7debca7802f
uhhangel <angelleeanna.b25@gmail.com>
2024-11-06 15:36:27 -0800

    fixed buttons on lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,22 +8,23 @@
 */
 
 // add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Click Me!</button>");
 
-// add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#challenge").append("<button id='button-challenge'>Make special</button>");
+$("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
+    console.log("clik");
 });
 
 // add button to problems section
-$("#problems").append("<button id='button-problems'>Click Me!</button>");
-    $("button-problems").click(function(){
+$("#problems").append("<button id='button-problems'>Make special</button>");
+    $("#button-problems").click(function(){
     $("#problems").toggleClass("special");
 });
 
 // add a button to results section
-$("#results").append("<button id='button-results'>Click Me!</button>");
-    $("button-results").click(function(){
+$("#results").append("<button id='button-results'>Make special</button>");
+    $("#button-results").click(function(){
      $("#results").toggleClass("special");
-});
+    });
+
