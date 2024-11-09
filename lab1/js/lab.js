8d3d77f7770ba6ef9e9440ff80dbdcdba7bf28e4
uhhangel <angelleeanna.b25@gmail.com>
2024-11-09 10:22:20 -0800

    lab 11!

--- lab1/js/lab.js
+++ lab1/js/lab.js
@@ -1,3 +1,6 @@
 var apples=5;
 console.log(apples);
 
+// make a button
+// add a listener
+// party mode
--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,6 +4,7 @@
 // Author: Angelleeanna Bingham
 // Date: 2024
 
+// referenced code from Wes Modes
 
 function generateRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
