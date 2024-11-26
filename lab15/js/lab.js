83ab3e9cfd0eacca8902caea18f5bbe6465b2f95
uhhangel <angelleeanna.b25@gmail.com>
2024-11-25 20:01:59 -0800

    lab 15 changes

---
+++ lab15/js/lab.js
@@ -0,0 +1,39 @@
+
+/*
+   lab.js - AJAX
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Angelleeanna Bingham
+   Date: 2024
+*/
+
+// yes or no api
+//"answer": "yes",
+//"forced": false,
+//"image": "https://yesno.wtf/assets/yes/2.gif"
+
+
+
+$("#mybutton").click(function() {
+    console.log("The button was clicked!");
+
+
+// Using the core $.ajax() method
+
+$.ajax({
+    //the url for the request (from the api docs)
+    url: "https://yesno.wtf/api",
+    //whether this is a POST or GET request
+    type: "GET",
+    dataType: "json",
+    //what do we do when the api call is successful
+    success: function(data) {
+        //Setting the image src attribute to the data
+    $("img[data-role=placeholder]").attr("src", data.image.image_url);
+        console.log(data);
+
+
+        },
+    });
+});
